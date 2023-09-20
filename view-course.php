95283b98214c91f14c742e160262a165852067c2
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-09-20 14:08:26 -0500

    Create view-course.php

---
+++ view-course.php
@@ -0,0 +1,11 @@
+<h1>Course</h1>
+<div class = table-responsive">
+  <table class = "table">
+    <thread>
+      <tr>
+        <th>ID</th>
+        <th>Number</th>
+        <th>Description</th>
+        <th></th>
+      </tr>
+    </thread>
