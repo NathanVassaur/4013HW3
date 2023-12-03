bebb4037d7d489afc18e66c83852832104dbe948
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 13:02:13 -0600

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -2,9 +2,8 @@
 require_once("util-db.php");
 require_once("model-instructors.php");
 
-
-include "view-header.php";
 $pageTitle = "Instructors";
+include "view-header.php";
 $instructors = selectInstructors();
 include "view-instructors.php";
 
