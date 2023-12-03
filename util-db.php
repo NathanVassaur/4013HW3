652b77b1f9bce12f82dd1c86244d9a8811f69bb1
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 12:24:30 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,1 +1,12 @@
-
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('138.68.62.201','nathanva_hw3user', 'MIS4013HW3', 'nathanva_hw3');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
