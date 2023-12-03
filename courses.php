51071049a51e884492814e8ecbe17eb6d254b6d5
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 13:08:19 -0600

    Create courses.php

---
+++ courses.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php")
+require_once("models-courses.php");
+
+$pageTitle = "Courses";
+include "view-header.php";
+$courses = selectCourses();
+include "view-courses.php";
+include "view-footer.php";
+?>
