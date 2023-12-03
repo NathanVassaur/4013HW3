55e7d33b2283893a4b9d6ea30a61b1d5a52334cf
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 17:56:12 -0600

    Update courses.php

--- courses.php
+++ courses.php
@@ -8,10 +8,13 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch($_POST['actionType']){
     case "Add":
-      insertCourse($_POST['cNumber'], $_POST['cDesc']);
-      break;
+      if(insertCourse($_POST['cNumber'], $_POST['cDesc'])){
+        echo'<div class="alert alert-success" role="alert">Course Added</div>'
+          } else{<div class="alert alert-success" role="alert">Error.</div>'}
+      break;     
   }
 }
+
 $courses = selectCourses();
 include "view-courses.php";
 include "view-footer.php";
