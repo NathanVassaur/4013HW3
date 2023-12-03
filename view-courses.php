4e6520120615446d04adc8a0ec62d0d11324724d
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 14:17:32 -0600

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -3,24 +3,22 @@
   <table class="table">
     <thead>
       <tr>
-        <th>ID</th>
+      <th>ID</th>
       <th>Number</th>
       <th>Description</th>
         <th></th>
       </tr>
-      
     </thead>
     <tbody>
 <?php
 while($course = $courses->fetch_assoc()){
-  ?>
+?>
   <tr>
     <td><?php echo $course['course_id'];?></td>
     <td><?php echo $course['course_name'];?></td>
     <td><?php echo $course['course_description'];?></td>
     <td>
-      <form>
-        <form method="post" action="sections-by-course.php">
+      <form method="post" action="sections-by-course.php">
           <input type = "hidden" name="cid" value="<?php echo $course['course_id']; ?>">
           <button type="submit" class="btn btn-primary">Sections</button>
       </form>
