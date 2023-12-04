58bd7f699dfd2a8260bc1d0322cce4a17976c831
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-03 18:09:44 -0600

    Update courses.php

--- courses.php
+++ courses.php
@@ -14,7 +14,7 @@ if (isset($_POST['actionType'])){
         echo '<div class="alert alert-danger" role="alert">Error. </div>';
       }
       break;
-    case "Add":
+    case "Delete":
       if(deleteCourse($_POST['cid'])){
         echo '<div class="alert alert-success" role="alert">Course deleted. </div>';
       } else {
