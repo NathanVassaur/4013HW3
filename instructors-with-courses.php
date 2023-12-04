dfd7c3429bb7babdcfb227665c2941dd24f199e4
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-04 13:52:18 -0600

    Update instructors-with-courses.php

--- instructors-with-courses.php
+++ instructors-with-courses.php
@@ -4,32 +4,6 @@ require_once("model-instructors-with-courses.php");
 
 $pageTitle = "Instructors With Courses";
 include "view-header.php";
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-      if (insertSection($_POST['iid'], $_POST['cid'], $_POST['sem'], $_POST['room'], $_POST['daytime'])) {
-        echo '<div class="alert alert-success" role="alert">Section added.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Edit":
-      if (updateSection($_POST['iid'], $_POST['cid'], $_POST['sem'], $_POST['room'], $_POST['daytime'], $_POST['sid'])) {
-        echo '<div class="alert alert-success" role="alert">Section edited.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Delete":
-      if (deleteSection($_POST['sid'])) {
-        echo '<div class="alert alert-success" role="alert">Section deleted.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-  }
-}
-
 $instructors = selectInstructors();
 include "view-instructors-with-courses.php";
 include "view-footer.php";
