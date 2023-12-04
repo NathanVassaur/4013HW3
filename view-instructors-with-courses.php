704444e9768fa7115c24c3abc946aa833f207945
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-04 00:01:36 -0600

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -1,49 +1,33 @@
-<!-- Button trigger modal -->
-<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#newSectionModal">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-    </svg>
-</button>
+<h1>Instructors With Courses</h1>
+<div class="card-group">
 
-<!-- Modal -->
-<div class="modal fade" id="newSectionModal" tabindex="-1" aria-labelledby="newSectionModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newSectionModalLabel">New section</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form method="post" action="">
-          <div class="mb-3">
-            <label for="iid" class="form-label">Instructor</label>
+
 <?php
-$instructorList = selectInstructorsForInput();
-$selectedInstructor = 0;
-include "instructor-input-list.php";
+while($instructor = $instructors->fetch_assoc()){
+  ?>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $instructor['instructor_name'];?></h5>
+      <p class="card-text">
+      <ul class="list-group">
+  <?php
+  $courses = selectCoursesByInstructor($instructor['instructor_id']);
+  
+  while($course = $courses->fetch_assoc()){
 ?>
-          </div>
-          <div class="mb-3">
-            <label for="cid" class="form-label">Course</label>
-            <input type="text" class="form-control" id="cid" name="cid">
-          </div>
-          <div class="mb-3">
-            <label for="sem" class="form-label">Semester</label>
-            <input type="text" class="form-control" id="sem" name="sem">
-          </div>
-          <div class="mb-3">
-            <label for="room" class="form-label">Room</label>
-            <input type="text" class="form-control" id="room" name="room">
-          </div>
-          <div class="mb-3">
-            <label for="daytime" class="form-label">Day/ time</label>
-            <input type="text" class="form-control" id="daytime" name="daytime">
-          </div>
-            <input type="hidden" name="actionType" value="Add">
-          <button type="submit" class="btn btn-primary">Save</button>
-        </form>
-      </div>
+    <li class="list-group-item"><?php echo $course['course_name'];?> - <?php echo $course['semester'];?> - <?php echo $course['room'];?> - <?php echo $course['day_time'];?></li>
+<?php
+    
+  }
+  
+  ?>
+      </ul>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Office: <?php echo $instructor['office_number'];?></small></p>
     </div>
   </div>
+  <?php
+}
+?>
 </div>
+
